5484566c480140b4a2b5ec1385200ad2c4877350
Kevin Stein <kevin.stein@chamaeleon.de>
2018-02-21 14:06:30 +0100

    BloqqPosts Tests

--- src/main/java/bbsmt/bloqq/bloqq/controller/BloqqPostController.java
+++ src/main/java/bbsmt/bloqq/bloqq/controller/BloqqPostController.java
@@ -6,20 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-/**
- * Created by stein on 14.02.2018.
- */
-
 @Controller
-@RequestMapping("/bloqqposts/")
+@RequestMapping("/bp")
 public class BloqqPostController {
 
     @Autowired
     private BloqqRepository bloqqRepository;
 
+
     public BloqqPostController(BloqqRepository bloqqRepository) {
         this.bloqqRepository = bloqqRepository;
     }
+
 
     @GetMapping("/")
     public Iterable<BloqqPost> list(){
