77015618b1307f27e873e09942840ba26229615d
Kevin Stein <kevin.stein@chamaeleon.de>
2018-03-07 10:31:41 +0100

    stuff

--- src/main/java/bbsmt/bloqq/bloqq/controller/BloqqPostController.java
+++ src/main/java/bbsmt/bloqq/bloqq/controller/BloqqPostController.java
@@ -1,14 +1,25 @@
 package bbsmt.bloqq.bloqq.controller;
 
 import bbsmt.bloqq.bloqq.entities.BloqqPost;
+import bbsmt.bloqq.bloqq.models.PageModel;
 import bbsmt.bloqq.bloqq.repository.BloqqRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/bp")
 public class BloqqPostController {
+
+    private static final int BUTTONS_TO_SHOW = 3;
+    private static final int INITIAL_PAGE = 0;
+    private static final int INITIAL_PAGE_SIZE = 1;
+    private static final int[] PAGE_SIZES = { 1,2,3 };
 
     @Autowired
     private BloqqRepository bloqqRepository;
@@ -18,9 +29,33 @@ public class BloqqPostController {
         this.bloqqRepository = bloqqRepository;
     }
 
+    @GetMapping("/all")
+    public ModelAndView bloqqPosts(@RequestParam("pageSize") Optional<Integer> pageSize,
+                                 @RequestParam("page") Optional<Integer> page){
 
-    @GetMapping("/")
-    public Iterable<BloqqPost> list(){
-        return bloqqRepository.findAll();
+        ModelAndView modelAndView = new ModelAndView("multipleBloqqPosts");
+
+        int evalPageSize = pageSize.orElse(INITIAL_PAGE_SIZE);
+        int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
+        Page<BloqqPost> bloqqlist = bloqqRepository.findAll( new PageRequest(evalPage, evalPageSize));
+        PageModel pager = new PageModel(bloqqlist.getTotalPages(),bloqqlist.getNumber(),BUTTONS_TO_SHOW);
+        // add clientmodel
+        modelAndView.addObject("bloqqlist",bloqqlist);
+        // evaluate page size
+        modelAndView.addObject("selectedPageSize", evalPageSize);
+        // add page sizes
+        modelAndView.addObject("pageSizes", PAGE_SIZES);
+        // add pager
+        modelAndView.addObject("pager", pager);
+        return modelAndView;
+    }
+
+    @GetMapping("/id/{id}")
+    public ModelAndView singleBloqqPost(@PathVariable int id){
+
+        ModelAndView modelAndView = new ModelAndView("singleBloqqPost");
+
+        modelAndView.addObject("bloqqpost",bloqqRepository.findById(id));
+        return modelAndView;
     }
 }
--- src/main/java/bbsmt/bloqq/bloqq/repository/BloqqRepository.java
+++ src/main/java/bbsmt/bloqq/bloqq/repository/BloqqRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BloqqRepository extends PagingAndSortingRepository<BloqqPost, Integer> {
-
+    public BloqqPost findById(int id);
 }
