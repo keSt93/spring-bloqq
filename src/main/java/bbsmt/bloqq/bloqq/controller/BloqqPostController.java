876be5ef11b7abc9ba346f8208af2bd9a9885610
Kevin Stein <kevin.stein@chamaeleon.de>
2018-03-13 12:24:06 +0100

    beauty makeover pt.2

--- src/main/java/bbsmt/bloqq/bloqq/controller/BloqqPostController.java
+++ src/main/java/bbsmt/bloqq/bloqq/controller/BloqqPostController.java
@@ -18,8 +18,8 @@ public class BloqqPostController {
 
     private static final int BUTTONS_TO_SHOW = 3;
     private static final int INITIAL_PAGE = 0;
-    private static final int INITIAL_PAGE_SIZE = 1;
-    private static final int[] PAGE_SIZES = { 1,2,3 };
+    private static final int INITIAL_PAGE_SIZE = 5;
+    private static final int[] PAGE_SIZES = { 5,10,20 };
 
     @Autowired
     private BloqqRepository bloqqRepository;
