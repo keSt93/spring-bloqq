969b46d3bd0ea33ff7b27c0b333c93bcdf6e66ee
Fabian Will <fabian2911@gmail.com>
2018-04-05 15:09:17 +0200

    Read properties from application

--- src/main/java/bbsmt/bloqq/bloqq/SpringSecurityConfig.java
+++ src/main/java/bbsmt/bloqq/bloqq/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,6 +26,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private AccessDeniedHandler accessDeniedHandler;
+
+    @Autowired
+    private Environment environment;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -51,9 +55,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
         DriverManagerDataSource userSource = new DriverManagerDataSource();
         userSource.setDriverClassName("com.mysql.jdbc.Driver");
-        userSource.setUrl("jdbc:mysql://localhost:3306/bloqq?useUnicode=true&amp;characterEncoding=UTF-8");
-        userSource.setUsername("root");
-        userSource.setPassword("root");
+        userSource.setUrl(environment.getProperty("spring.datasource.url"));
+        userSource.setUsername(environment.getProperty("spring.datasource.username"));
+        userSource.setPassword(environment.getProperty("spring.datasource.password"));
 
         auth.jdbcAuthentication()
                 .dataSource(userSource)
